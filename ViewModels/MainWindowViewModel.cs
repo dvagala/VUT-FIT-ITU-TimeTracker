e44fc7bacfb651d5efc8f945bf5423284da0bf21
Dominik Vagala <d.vagala@gmail.com>
2019-12-08 18:59:14 +0100

    add comments

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// View model for whole app. Author: Dominik Vagala
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
